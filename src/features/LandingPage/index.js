42273ac7c418197e91fe37b61ca8652f7a7d4f05
James <j606888@gmail.com>
2025-09-19 15:00:07 +0800

    Add Contact

--- src/features/LandingPage/index.js
+++ src/features/LandingPage/index.js
@@ -1,6 +1,7 @@
 import Hero from "./Hero";
 import Service from "./Service";
 import Portfolio from "./Portfolio";
+import Contact from "./Contact";
 
 const LandingPage = () => {
   return (
@@ -8,6 +9,10 @@ const LandingPage = () => {
       <Hero />
       <Service />
       <Portfolio />
+      <Contact />
+      <div className="flex items-center justify-center py-4">
+        <p className="text-[#D4D4D4] text-sm">Copyright © 2025 James Lees</p>
+      </div>
     </>
   );
 };
